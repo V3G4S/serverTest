3321fb14343f2cb2920459a264495fda7bcb1f8f
ANTONY VALENTIM GARCIA DOS SANTOS <antony.equisul@gmail.com>
2025-04-02 09:54:39 -0300

    [ADDING] New functions

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const {soma} = require('./teste');
+const {soma, subtrair, multiplicar, dividir} = require('./teste');
 const PORT = 8000;
 const url = require('url');
 
@@ -7,9 +7,21 @@ const server = http.createServer((req, res) => {
     res.setHeader('Content-Type', 'text/plain; charset=utf-8'); // Serve pra utilizar a tabela ASCII -8 
     // (que suporta caracteres especiais)
     const urlCapturada = url.parse(req.url, true)
-    const {query} = urlCapturada
-    let resultado = soma(Number(query.a), Number(query.b))
-    res.end(`Resultado da soma: ${resultado}
+    const {query, pathname} = urlCapturada
+    let resultado = 0
+    if (pathname == '/soma'){
+        resultado = soma(Number(query.a), Number(query.b))
+    }else if (pathname == '/subtrair'){
+        resultado = subtrair(Number(query.a), Number(query.b))
+    }else if (pathname == '/multiplicar'){
+        resultado = multiplicar(Number(query.a), Number(query.b))
+    }else if (pathname == '/dividir'){
+        resultado = dividir(Number(query.a), Number(query.b))
+    }else{
+        res.statusCode = 404
+        res.end('Página não encontrada')
+    }
+    res.end(`Resultado do cálculo: ${resultado}
         para trocar os valores, utilize o url do site (ex: localhost:8000/?a=10&b=20)`);
     });
     
--- teste.js
+++ teste.js
@@ -3,4 +3,16 @@ function soma (a,b){
     return a + b;
 }
 
-module.exports = {soma}
+function subtrair (a,b){
+    return a - b;
+}
+
+function multiplicar (a,b){
+    return a * b;
+}
+
+function dividir (a,b){
+    return a / b;
+}
+
+module.exports = {soma, subtrair, multiplicar, dividir}
